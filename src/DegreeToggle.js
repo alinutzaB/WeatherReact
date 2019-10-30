01c413a5feb7f60ad28b3accbeffde487dde3682
alinutzaB <alinutza.ms@gmail.com>
2019-10-30 23:15:01 +0200

    Weather React App

---
+++ src/DegreeToggle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+	const DegreeToggle = ({degreeType, updateForecastDegree}) => {
+		return (
+			<React.Fragment>
+				<div className="form-check form-check-inline">
+					<input className="form-check-input"	type="radio" name="degree-type"	id="celsius" value="celsius" defaultChecked={degreeType === "celsius"}	onChange={updateForecastDegree}	/>
+					<label className="form-check-label" htmlFor="celsius">Celsius</label>
+				</div>
+				<div className="form-check form-check-inline">
+					<input className="form-check-input" type="radio" name="degree-type"	id="fahrenheit" value="fahrenheit" defaultChecked={degreeType === "fahrenheit"}	onChange={updateForecastDegree}	/>
+					<label className="form-check-label" htmlFor="fahrenheit">Farenheit</label>
+				</div>
+			</React.Fragment>
+		)
+	}
+
+export default DegreeToggle;
