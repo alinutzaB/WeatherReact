e6fd1b7a837b1708cae26ff536320b3f80578b0f
alinutzaB <alinutza.ms@gmail.com>
2019-10-31 22:50:56 +0200

    Updates

---
+++ precache-manifest.a50abab28b2071c0c0c6258885aafda4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f765aebca6fee1b3f0a52771467c4c3c",
+    "url": "/WeatherReact/index.html"
+  },
+  {
+    "revision": "758d134b7af9f22f614e",
+    "url": "/WeatherReact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fd537d5dc0ba2d059aee",
+    "url": "/WeatherReact/static/js/2.74c68b12.chunk.js"
+  },
+  {
+    "revision": "758d134b7af9f22f614e",
+    "url": "/WeatherReact/static/js/main.8e9888d5.chunk.js"
+  },
+  {
+    "revision": "200ec4e22d7ed469d1f0",
+    "url": "/WeatherReact/static/js/runtime-main.f4c6700e.js"
+  }
+]);
